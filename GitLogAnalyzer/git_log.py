b20d43d35689559ff7cc5282cb5dfae0fb669694
lucho <lucho@LENOVOS400>
2017-03-30 08:30:13 -0300

    Added lines count logic

--- GitLogAnalyzer/git_log.py
+++ GitLogAnalyzer/git_log.py
@@ -4,6 +4,8 @@ commit_count = 0
 authors = []
 authors_commits = {}
 fix_and_bugs_count = 0
+lines_count = 0
+
 
 def get_counts(log_file):
     gitlog_file = open(log_file, "r", encoding="utf8")
@@ -14,15 +16,20 @@ def get_counts(log_file):
     authors = get_authors(gitlog_text)
     authors_commits = get_commits_by_author(gitlog_text, authors)
     fix_and_bugs_count = count_fix_and_bugs(gitlog_text)
+    lines_count = count_lines(gitlog_text)
     
     print("Total commits:" + str(commit_count))
+    print("Total project lines:" + str(lines_count))
+    print("Bugs / Code Lines ratio:" + '{0:.2f}'.format(fix_and_bugs_count / lines_count * 100) + "%")
+
     print("Number of commits by developer:")
     for author in authors:
         print(author + ":" + str(authors_commits[author]))
 
     print("Feature Commits:" + str(commit_count - fix_and_bugs_count))
     print("Commits including Fix or Bug:" + str(fix_and_bugs_count))
-    print("Bugs / Feature Commits ratio:" + str(fix_and_bugs_count / commit_count * 100))
+    print("Bugs / Feature Commits ratio:" + str('{0:.2f}'.format(fix_and_bugs_count / commit_count * 100)) + "%")
+
 
 def count_commits(gitlog_text):
     commit_count = 0    
@@ -66,9 +73,40 @@ def get_commits_by_author(gitlog_text, authors):
 
 def count_fix_and_bugs(gitlog_text):
     fix_and_bugs_count = 0    
+    index = -1
 
-    for line in gitlog_text.split('\n'):
-        if "FIX" in line.upper() or "BUG" in line.upper(): 
-            fix_and_bugs_count += 1
+    lines = gitlog_text.split('\n')
+    while index < (len(lines) - 1):
+        index += 1
+        line = lines[index]
+        if line.startswith("Date:"):
+            index += 2
+            line = lines[index]
+            if "FIX" in line.upper() or "BUG" in line.upper(): 
+                fix_and_bugs_count += 1        
+        pass
 
     return fix_and_bugs_count
+
+def count_lines(gitlog_text):
+    lines_count = 0
+    index = -1
+    added_lines = 0
+    removed_lines = 0
+
+    lines = gitlog_text.split('\n')
+    while index < (len(lines) - 1):
+        index += 1
+        line = lines[index]
+        if "files changed" in line:
+            parts = line.split(',')
+            for part in parts:
+                if "insertions" in part:
+                    added_lines_str = part.split(' ')[1]
+                    added_lines += int(added_lines_str)
+                if "deletions" in part:
+                    removed_lines_str = part.split(' ')[1]
+                    removed_lines += int(removed_lines_str)
+    lines_count = added_lines - removed_lines
+
+    return lines_count
--- GitLogAnalyzer/main.py
+++ GitLogAnalyzer/main.py
@@ -1,3 +1,6 @@
 import git_log as gl
 
+#To get the log run the following command inside the project:
+#git log --stat
+
 gl.get_counts(r"C:\Users\lucho\Documents\gitlog.txt")
